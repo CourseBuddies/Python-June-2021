86e6b1d6a48f22e8e00ed8a7dca39eee475247b7
Necip Fazıl Akay <54453386+necpfzlakay@users.noreply.github.com>
2021-07-19 19:41:41 +0300

    Delete Week_1-1.py

--- Week_1-1.py
+++
@@ -1,8 +0,0 @@
-isim = input('Lütfen isminizi giriniz:--> ')
-yas = int(input('Lütfen Yaşınızı Giriniz:--> '))
-ortalama = int(input('Ortalamanızı giriniz:--> '))
-
-if(yas>=17 and yas<22 and ortalama>=80):
-    print(isim + ': geçti')
-else: 
-    print(isim + ': kaldı')
