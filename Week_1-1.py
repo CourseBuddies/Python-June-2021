ea18b036ddd3074f2f3dd955559c7c666694dd3f
Necip <akaynecip@gmail.com>
2021-07-20 00:55:42 +0300

    19.07.2021 Lab Dersi Soru Çözümü

--- Week_1-1.py
+++ Week_1-1.py
@@ -1,8 +1,8 @@
-isim = input('Lütfen isminizi giriniz:--> ')
-yas = int(input('Lütfen Yaşınızı Giriniz:--> '))
-ortalama = int(input('Ortalamanızı giriniz:--> '))
+isim = input("İsminizi giriniz: ")
+yas = int(input("Yaşınız giriniz: "))
+ortalama = int(input("Not ortalamanızı giriniz: "))
 
-if(yas>=17 and yas<22 and ortalama>=80):
-    print(isim + ': geçti')
-else: 
-    print(isim + ': kaldı')
+if (yas>=17 and yas<=21 and ortalama >= 80 and ortalama <= 100):
+    print("Sınıfı Geçti")
+else:
+    print("Sınıfı Geçemedi")
